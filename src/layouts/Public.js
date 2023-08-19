b2ebc1c318af0f102691889cf63b31f9148d0f05
Antonio Cruz Hernandez <andejecruher@gmail.com>
2023-08-19 12:23:02 -0600

    update layout

--- src/layouts/Public.js
+++ src/layouts/Public.js
@@ -4,16 +4,12 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import CssBaseline from '@mui/material/CssBaseline';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import { Container } from '@mui/material';
 
-const drawerWidth = 470;
+const drawerWidth = 435;
 
 const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
@@ -43,19 +39,29 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   justifyContent: 'flex-start',
 }));
 
+const DrawerFooter = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  justifyContent: 'flex-start',
+}));
+
+const DrawerBody = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  justifyContent: 'flex-start',
+}));
+
 export default function PersistentDrawerLeft() {
   const [open] = useState(true);
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <Drawer
+      <Drawer 
         sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
+          width: drawerWidth,   
+          backgroundColor: '#f6f1f0',
         }}
         variant="persistent"
         anchor="left"
@@ -63,21 +69,29 @@ export default function PersistentDrawerLeft() {
       >
         <Container>
           <DrawerHeader>
-            <h1>Logo</h1>
+            <div>
+              <img width="200" src="https://i.postimg.cc/hPW6L8Ds/kisspng-wedding-invitation-bridegroom-clip-art-bride-and-groom-5b10fe3602bda4-1847344215278403100112.png" alt="logo" />
+              <h1>Valeria  & Orlando</h1>
+              <p>15.11.2023</p>
+            </div>
           </DrawerHeader>
-          <Divider />
+          <DrawerBody>
           <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            {['Inicio', 'Bienvenida', 'Ceremonia y Recepción', 'Código de vestimenta', 'Pase', 'Mesa de regalos', 'Confirmación'].map((text, index) => (
               <ListItem key={text} disablePadding>
                 <ListItemButton>
-                  <ListItemIcon>
-                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                  </ListItemIcon>
                   <ListItemText primary={text} />
                 </ListItemButton>
               </ListItem>
             ))}
           </List>
+          </DrawerBody>
+          <DrawerFooter>
+            <div>
+              <p>© 2023 Valeria & Orlando. Boda</p>
+              <p>21 - Diciembre - 2023.</p>
+            </div>
+          </DrawerFooter>
         </Container>
       </Drawer>
       <Main open={open}>
